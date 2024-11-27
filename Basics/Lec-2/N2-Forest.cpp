a7f66869ac38e3b81c776ec7e70340d0d796e33d
Harshal Dulera <harshaldulera82@gmail.com>
2024-11-27 13:06:12 +0530

    add N-Forest and N2-Forest programs for generating star patterns; include VSCode settings for file associations

---
+++ Basics/Lec-2/N2-Forest.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI 3.14
+#define long long ll
+
+void nForest(int n) {
+    for(int i = 0; i < n; i++) {
+		for(int j = 0; j <= i; j++) {
+			cout << "*" << " ";
+		}
+	    cout << endl;
+	}
+}
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n; cin >> n;
+    nForest(n);
+
+    return 0;
+}
